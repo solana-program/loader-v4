cd1ce418141597729f90e09c38eb0a823418f27a
Joe Caulfield <joe.caulfield@anza.xyz>
2024-10-08 07:43:34 +0800

    processor: truncate

--- program/src/processor.rs
+++ program/src/processor.rs
@@ -11,6 +11,8 @@ use {
         msg,
         program_error::ProgramError,
         pubkey::Pubkey,
+        rent::Rent,
+        sysvar::Sysvar,
     },
 };
 
@@ -96,11 +98,85 @@ fn process_write(
 /// Processes a
 /// [Truncate](enum.LoaderV4Instruction.html)
 /// instruction.
-fn process_truncate(
-    _program_id: &Pubkey,
-    _accounts: &[AccountInfo],
-    _new_size: u32,
-) -> ProgramResult {
+fn process_truncate(program_id: &Pubkey, accounts: &[AccountInfo], new_size: u32) -> ProgramResult {
+    let accounts_iter = &mut accounts.iter();
+
+    let program_info = next_account_info(accounts_iter)?;
+    let authority_info = next_account_info(accounts_iter)?;
+
+    let is_initialization =
+        new_size > 0 && program_info.data_len() < LoaderV4State::program_data_offset();
+
+    if is_initialization {
+        if program_info.owner != program_id {
+            msg!("Program not owned by loader");
+            return Err(ProgramError::InvalidAccountOwner);
+        }
+        if !program_info.is_writable {
+            msg!("Program is not writeable");
+            return Err(ProgramError::InvalidArgument);
+        }
+        if !program_info.is_signer {
+            msg!("Program did not sign");
+            return Err(ProgramError::MissingRequiredSignature);
+        }
+        if !authority_info.is_signer {
+            msg!("Authority did not sign");
+            return Err(ProgramError::MissingRequiredSignature);
+        }
+    } else {
+        let state = check_program_account(program_id, program_info, authority_info)?;
+        if !matches!(state.status, LoaderV4Status::Retracted) {
+            msg!("Program is not retracted");
+            return Err(ProgramError::InvalidArgument);
+        }
+    }
+
+    let required_lamports = if new_size == 0 {
+        0
+    } else {
+        let rent = <Rent as Sysvar>::get()?;
+        rent.minimum_balance(LoaderV4State::program_data_offset().saturating_add(new_size as usize))
+            .max(1)
+    };
+
+    match program_info.lamports().cmp(&required_lamports) {
+        std::cmp::Ordering::Less => {
+            msg!("Insufficient lamports, {} are required", required_lamports);
+            return Err(ProgramError::InsufficientFunds);
+        }
+        std::cmp::Ordering::Greater => {
+            let destination_info = next_account_info(accounts_iter)?;
+            if !destination_info.is_writable {
+                msg!("Recipient is not writeable");
+                return Err(ProgramError::InvalidArgument);
+            }
+            let lamports_to_receive = program_info.lamports().saturating_sub(required_lamports);
+            let new_destination_lamports = destination_info
+                .lamports()
+                .saturating_add(lamports_to_receive);
+            **program_info.try_borrow_mut_lamports()? = required_lamports;
+            **destination_info.try_borrow_mut_lamports()? = new_destination_lamports;
+        }
+        std::cmp::Ordering::Equal => {}
+    }
+
+    if new_size == 0 {
+        program_info.realloc(0, true)?;
+    } else {
+        program_info.realloc(
+            LoaderV4State::program_data_offset().saturating_add(new_size as usize),
+            true,
+        )?;
+        if is_initialization {
+            let mut data = program_info.try_borrow_mut_data()?;
+            let state = LoaderV4State::unpack_mut(&mut data)?;
+            state.slot = 0;
+            state.status = LoaderV4Status::Retracted;
+            state.authority_address_or_next_version = *authority_info.key;
+        }
+    }
+
     Ok(())
 }
 
