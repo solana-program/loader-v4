698906089087b489e98c2548fbaef5993de0a7da
Joe Caulfield <joe.caulfield@anza.xyz>
2024-12-04 16:49:52 +0900

    use the real program id

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -7,7 +7,4 @@ pub mod instruction;
 pub mod processor;
 pub mod state;
 
-// [CORE BPF]: Unfortunately, the runtime still depends pretty heavily on this
-// program ID hard-coded, so we can't test with it just yet.
-// solana_program::declare_id!("LoaderV411111111111111111111111111111111111");
-solana_program::declare_id!("CoreBPFLoaderV41111111111111111111111111111");
+solana_program::declare_id!("LoaderV411111111111111111111111111111111111");
